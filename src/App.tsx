1b456e63839bb50d0e404873e362551474a112df
Berenger Dalle-Cort <berenger@42borgata.com>
2026-04-21 10:33:11 -0400

    refactor: move icons to Icons.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import packageJson from "../package.json"
 import { useCallback, useEffect, useMemo, useState, type MouseEventHandler, type ReactNode } from "react"
 import { ValueInputField } from "./ValueInputField"
 import { MODE, type Mode } from "./Metronome/Metronome"
+import { UpIcon, UpDownIcon, StopIcon, PlayIcon } from "./Icons"
 
 // TODO LIST
 //
@@ -34,9 +35,7 @@ const MODES: Array<{ value: Mode, label: ReactNode }> = [
     value: MODE.INTERPOLATE_UP_AND_HOLD,
     label:
       <div className="d-flex">
-        <svg width="1rem" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 6.75 12 3m0 0 3.75 3.75M12 3v18" />
-        </svg>
+        <UpIcon/>
         <span>Accelerando</span>
       </div>
   },
@@ -44,21 +43,11 @@ const MODES: Array<{ value: Mode, label: ReactNode }> = [
     value: MODE.INTERPOLATE_UP_AND_DOWN_FOREVER,
     label:
       <div className="d-flex">
-        <svg width="1rem" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
-        </svg>
+        <UpDownIcon/>
         <span>Yoyo</span>
       </div>
   },
 ]
-
-const playIcon = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-  <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm14.024-.983a1.125 1.125 0 0 1 0 1.966l-5.603 3.113A1.125 1.125 0 0 1 9 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113Z" clipRule="evenodd" />
-</svg>
-
-const stopIcon = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-  <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm6-2.438c0-.724.588-1.312 1.313-1.312h4.874c.725 0 1.313.588 1.313 1.313v4.874c0 .725-.588 1.313-1.313 1.313H9.564a1.312 1.312 0 0 1-1.313-1.313V9.564Z" clipRule="evenodd" />
-</svg>
 
 
 
@@ -136,7 +125,7 @@ function App() {
 
             <div className="flex-col">
               <button className='play-button' onClick={handlePlayButtonClick}>
-                { state.isPlaying ? <>{stopIcon} STOP</> : <>{playIcon} PLAY</> }
+                { state.isPlaying ? <><StopIcon/> STOP</> : <><PlayIcon/> PLAY</> }
               </button>
               <div className="volume">
                 <input
---
+++ src/Icons.tsx
@@ -0,0 +1,34 @@
+//
+// Some inlined SVG icons
+//
+
+const PlayIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+        <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm14.024-.983a1.125 1.125 0 0 1 0 1.966l-5.603 3.113A1.125 1.125 0 0 1 9 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113Z" clipRule="evenodd" />
+    </svg>
+)
+
+const StopIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+    <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm6-2.438c0-.724.588-1.312 1.313-1.312h4.874c.725 0 1.313.588 1.313 1.313v4.874c0 .725-.588 1.313-1.313 1.313H9.564a1.312 1.312 0 0 1-1.313-1.313V9.564Z" clipRule="evenodd" />
+    </svg>
+)
+
+const UpIcon = () => (
+    <svg width="1rem" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 6.75 12 3m0 0 3.75 3.75M12 3v18" />
+    </svg>
+)
+
+const UpDownIcon = () => (
+    <svg width="1rem" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
+    </svg>
+)
+
+export {
+    PlayIcon,
+    StopIcon,
+    UpIcon,
+    UpDownIcon
+}
